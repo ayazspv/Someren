ee4824543b7642a4350cd384674ea2b8e404eb4d
tnb1604 <damisavooges2549@gmail.com>
2024-03-26 12:31:01 +0100

    remove methods with 0 references

--- SomerenUI/SomerenUI.cs
+++ SomerenUI/SomerenUI.cs
@@ -410,57 +410,7 @@ namespace SomerenUI
                 lblSelectedStudent.Text = "Selected Student: " + selectedStudentFirstName;
             }
         }
-        private void placeOrderButton_Click(object sender, EventArgs e)
-        {
-            /*if (listViewSelectStudent.SelectedItems.Count == 0 || listViewSelectDrink.SelectedItems.Count == 0 ||
-                !int.TryParse(textBoxSelectDrinkQuantity.Text, out int quantity))
-            {
-                MessageBox.Show("Please make sure to select a student, a drink, and enter a valid quantity.");
-                return;
-            }
-
-            // Get the selected student
-            ListViewItem selectedStudentItem = listViewSelectStudent.SelectedItems[0];
-            int selectedStudentNumber = int.Parse(selectedStudentItem.SubItems[0].Text);
-
-            // Get the selected drink
-            ListViewItem selectedDrinkItem = listViewSelectDrink.SelectedItems[0];
-            int selectedDrinkNumber = int.Parse(selectedDrinkItem.SubItems[0].Text);
-
-            // Get the selected drink and calculate total price
-            Drink selectedDrink = GetDrinks().Find(drink => drink.DrinkNumber == selectedDrinkNumber);
-            double totalPrice = selectedDrink.Price * quantity;
-
-            // Ensure there is enough stock for the order
-            if (selectedDrink.Stock < quantity)
-            {
-                MessageBox.Show("Not enough stock available for this drink and quantity.");
-                return;
-            }
-
-            // Store the sold drink in the database
-            OrdersService ordersService = new OrdersService();
-            ordersService.AddDrinkSale(new Orders
-            {
-                DrinkNumber = selectedDrinkNumber,
-                StudentNumber = selectedStudentNumber,
-                Quantity = quantity,
-                TotalPrice = totalPrice,
-                PurchaseDateTime = DateTime.Now
-            });
-
-            // Decrease the stock amount of the sold drink
-            DrinkService drinkService = new DrinkService();
-            int updatedStock = selectedDrink.Stock - quantity;
-            if (updatedStock < 0)
-            {
-                updatedStock = 0; // Ensure stock does not go below 0
-            }
-            //drinkService.UpdateDrinkStock(selectedDrinkNumber, updatedStock);
-
-            MessageBox.Show("Order placed successfully!");
-            RefreshDrinkOrderPanel();*/
-        }
+       
         void RefreshDrinkOrderPanel()
         {
             listViewSelectDrink.SelectedItems.Clear();
@@ -498,31 +448,6 @@ namespace SomerenUI
                 listViewSelectDrink.Items.Add(item);
             }
         }
-        private void button1_Click(object sender, EventArgs e)
-        {
-            /*if (listViewSelectDrink.SelectedItems.Count == 0)
-            {
-                MessageBox.Show("Please select a drink.");
-                return;
-            }
-            if (!int.TryParse(textBoxSelectDrinkQuantity.Text, out int quantity))
-            {
-                MessageBox.Show("Please enter a valid quantity.");
-                return;
-            }
-
-            // Get the selected drink
-            ListViewItem selectedDrinkItem = listViewSelectDrink.SelectedItems[0];
-            int selectedDrinkNumber = int.Parse(selectedDrinkItem.SubItems[0].Text);
-
-            // Get the selected drink and calculate total price
-            Drink selectedDrink = GetDrinks().Find(drink => drink.DrinkNumber == selectedDrinkNumber);
-            double totalPrice = selectedDrink.Price * quantity;
-
-            // Display total price in label6
-            lblShowTotalPrice.Text = "Total Price: �" + totalPrice.ToString("0.00");*/
-        }
-
 
         private void buttonCalcCost_Click_1(object sender, EventArgs e)
         {
@@ -673,18 +598,6 @@ namespace SomerenUI
         {
 
         }
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
         private void label5_Click(object sender, EventArgs e)
         {
 
@@ -703,11 +616,6 @@ namespace SomerenUI
         }
 
         private void buttonCalcCost_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void placeOrderButton_Click_1(object sender, EventArgs e)
         {
 
         }
